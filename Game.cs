ee6a9de3bd4bd77abf60b7c4b3a0dbc87fc3494d
Say Cheong <greymistcube@gmail.com>
2022-04-22 16:46:15 +0900

    File path handling

--- Game.cs
+++ Game.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using Util;
 
 public class Game : Node2D
 {
@@ -10,7 +11,11 @@ public class Game : Node2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-
+        GD.Print("Game started");
+        GD.Print($"Genesis block path: {FileManager.GenesisBlockPath}");
+        GD.Print($"Private key path: {FileManager.PrivateKeyPath}");
+        GD.Print($"Store path: {FileManager.StorePath}");
+        GD.Print($"State store path: {FileManager.StateStorePath}");
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
